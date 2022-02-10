925be929397e7bf2c6d5c6e6be6a76f41fdd1ab2
Laurin Muth <97051027+WauWauGirly@users.noreply.github.com>
2022-02-10 19:56:08 +0100

    Add music and credits

--- app/main.rb
+++ app/main.rb
@@ -17,6 +17,7 @@ class State
   INTRO = 0
   COUNTDOWN = 1
   GAME = 2
+  CREDITS = 3
 end
 
 def inside_sprite(mouse_x, mouse_y, size, x_position, y_position)
@@ -27,6 +28,15 @@ def inside_sprite(mouse_x, mouse_y, size, x_position, y_position)
 end
 
 def tick(args)
+
+    if args.state.tick_count == 0
+      args.audio[:my_audio] = {
+        input: 'sounds/music/music.ogg',
+        gain: 0.3,
+        looping: true
+      }
+    end
+
   @args = args
   @state ||= State::INTRO
 
@@ -34,10 +44,38 @@ def tick(args)
   when State::INTRO
     intro
     @state = State::COUNTDOWN if @args.inputs.keyboard.key_down.space
+    @state = State::CREDITS if @args.inputs.keyboard.key_down.c
   when State::COUNTDOWN
     countdown
   when State::GAME
     game
+  when State::CREDITS
+    credits
+  end
+end
+
+def draw_all_raccoons
+  sin = Math.sin(@args.state.tick_count / 60)
+  sin = sin.clamp(0, 1).ceil
+  sin_2 = Math.sin(@args.state.tick_count / 2)
+  sin *= sin_2
+  draw_raccoon(0, 0, false, sin)
+  draw_raccoon(0, HEIGHT - RACCOON_HEIGHT * RACCOON_SIZE, false, sin)
+  draw_raccoon(WIDTH - RACCOON_WIDTH * RACCOON_SIZE, HEIGHT - RACCOON_HEIGHT * RACCOON_SIZE, true, sin)
+  draw_raccoon(WIDTH - RACCOON_WIDTH * RACCOON_SIZE, 0, true, sin)
+end
+
+def credits
+  draw_all_raccoons
+  text = ['Code and Art by Lea (WauWauGirly)','Raccoon by Dodo','Music by Micah Young','With Help by lyniat']
+  i = 0
+  text.each do |t|
+    @args.outputs.labels << [WIDTH / 2, HEIGHT - 150 - i * 100, t, 60, 1, 0, 0, 150, 255, FONT]
+    i += 1
+  end
+
+  if @args.inputs.mouse.click || @args.inputs.keyboard.key_down.space || @args.inputs.keyboard.key_down.escape
+    @state = State::INTRO
   end
 end
 
@@ -56,15 +94,10 @@ def draw_raccoon(x, y, flip, angle)
 end
 
 def intro
-  sin = Math.sin(@args.state.tick_count / 60)
-  sin = sin.clamp(0, 1).ceil
-  sin_2 = Math.sin(@args.state.tick_count / 2)
-  sin *= sin_2
   @args.outputs.sprites << [0, 0, WIDTH, HEIGHT, '/sprites/start_screen.png']
-  draw_raccoon(0, 0, false, sin)
-  draw_raccoon(0, HEIGHT - RACCOON_HEIGHT * RACCOON_SIZE, false, sin)
-  draw_raccoon(WIDTH - RACCOON_WIDTH * RACCOON_SIZE, HEIGHT - RACCOON_HEIGHT * RACCOON_SIZE, true, sin)
-  draw_raccoon(WIDTH - RACCOON_WIDTH * RACCOON_SIZE, 0, true, sin)
+  draw_all_raccoons
+  @args.outputs.labels << [640, 200, 'click space to start', 50, 1, 0, 0, 150, 255, FONT]
+  @args.outputs.labels << [640, 120, 'or c for credits', 20, 1, 0, 0, 150, 255, FONT]
 end
 
 def countdown
@@ -156,11 +189,12 @@ def draw_animal(i)
   inside = inside_sprite(mouse_x, mouse_y, @size, @animals[i].x_position, @animals[i].y_position)
 
   if @args.inputs.mouse.click && inside && !@game_over
+    # @args.outputs.sounds << '/sounds/click.wav'
     @size /= 1.1
     if @animals[i].enemy == false
-      @points += 1
+      @raccoon_clicked = true
     else
-      @points -= 1
+      @snake_clicked = true
     end
   end
 
@@ -179,6 +213,10 @@ def draw_animal(i)
 end
 
 def game
+
+  @raccoon_clicked = false
+  @snake_clicked = false
+
   @args.outputs.sprites << [0, 0, 1280, 720, '/sprites/background.png']
 
   i = 0
@@ -192,6 +230,12 @@ def game
 
   unless @game_over
     @args.outputs.sprites << [0, HEIGHT - 60, WIDTH, 60, '/sprites/blank.png', 0, 127]
+  end
+
+  if @snake_clicked
+    @points -= 1
+  elsif @raccoon_clicked
+    @points += 1
   end
 
   @args.outputs.labels << [0, HEIGHT, "Points: #{@points}", 20, 0, 150, 0, 0, 255, FONT]
@@ -210,4 +254,7 @@ def game
     @args.outputs.labels << [WIDTH / 2, 450, "YOU GOT #{@points} POINTS!", 60, 1, 0, 0, 150, 255, FONT]
     @state = State::INTRO if @args.inputs.keyboard.key_down.space
   end
+
+  @raccoon_clicked = false
+  @snake_clicked = false
 end
